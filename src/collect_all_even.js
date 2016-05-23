6797618704ec781842ab0f3833a5b90084af75de
hang515161153 <515161153@qq.com>
2016-05-23 22:27:47 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,10 +1,14 @@
 'use strict';
 
 function collect_all_even(collection) {
-  	var pattern = /\d*[02468]/g;
+  var pattern = /\d*[02468]/g;
  	var collection = collection.join(",");
  	var arr = collection.match(pattern);
- 	return arr;
+ 	var Arr = [];
+ 	for(var i in arr){
+      		 Arr.push(arr[i]);
+    	}
+ 	return Arr;
 }
 
 module.exports = collect_all_even;
